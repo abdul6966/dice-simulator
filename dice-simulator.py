c52e2e12fba4fcda7d98eb365a9bf8ac332470e7
Abdul Nayeem <154670886+abdul6966@users.noreply.github.com>
2024-05-17 10:48:28 +0530

    Update dice-simulator.py

--- dice-simulator.py
+++ dice-simulator.py
@@ -1,1 +1,9 @@
-
+import random
+num = True
+while num:
+  print(random.randint(1,6))
+  another_num = input("Do you want to roll dice again? (y/n): ")
+  if another_num.lower() == "y":
+    continue
+  else:
+    break
